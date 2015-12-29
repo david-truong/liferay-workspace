e9beabafa890c368ed6652b2213f705ee2eecebc
Gregory Amerson <gregory.amerson@liferay.com>
2015-12-29 17:02:03 -0600

    Prepare release v0.3.0

--- plugins-sdk/portlets/sample-python-portlet/docroot/WEB-INF/python/action.py
+++
@@ -1,7 +0,0 @@
-numStr = preferences.getValue("number", "0");
-
-num = int(numStr) + 1;
-
-preferences.setValue("number", str(num));
-
-preferences.store();
--- themes/sample-theme/gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use stict';
-
-var gulp = require('gulp');
-var liferayThemeTasks = require('liferay-theme-tasks');
-
-liferayThemeTasks.registerTasks({
-	gulp: gulp
-});
