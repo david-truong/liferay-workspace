2a3c418a300c77ef65a96223fc459c2fd815d19e
David Truong <david.truong@liferay.com>
2016-01-13 13:33:48 -0600

    add a unit test

---
+++ modules/apps/sample/src/test/java/sample/SamplePortletTest.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by dtruong on 1/13/16.
+ */
+public class SamplePortletTest {
+
+	@Test
+	public void testSamplePortlet() throws Exception {
+		System.out.println("Making sure unit tests are running");
+
+		Assert.assertTrue(true);
+	}
+}
