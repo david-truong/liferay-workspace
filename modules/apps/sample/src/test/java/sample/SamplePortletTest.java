264f53cc3a4900b0a14abc7cae7b9c89ba4ef815
David Truong <david.truong@liferay.com>
2016-01-13 18:27:48 -0600

    improved tests

--- modules/apps/sample/src/test/java/sample/SamplePortletTest.java
+++ modules/apps/sample/src/test/java/sample/SamplePortletTest.java
@@ -1,17 +1,53 @@
 package sample;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
-/**
- * Created by dtruong on 1/13/16.
- */
 public class SamplePortletTest {
 
+	@Before
+	public void setUp() {
+		System.out.println("Invoking setUp()");
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		System.out.println("Invoking setUpBeforeClass()");
+	}
+
+	@After
+	public void tearDown() {
+		System.out.println("Invoking tearDown()");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() {
+		System.out.println("Invoking tearDownAfterClass()");
+	}
+
+	@Ignore
 	@Test
-	public void testSamplePortlet() throws Exception {
-		System.out.println("Making sure unit tests are running");
+	public void testIgnore() {
+		System.out.println("Test ignored");
+
+		Assert.assertTrue(false);
+	}
+
+	@Test
+	public void testSample() {
+		System.out.println("Sample Test");
 
 		Assert.assertTrue(true);
 	}
+
+	@Test(expected = RuntimeException.class)
+	public void testSampleThrowsException() {
+		throw new RuntimeException();
+	}
+
 }
