62f0a57fb1b12f15fddd2068ef1fccb8987e7453
David Truong <david.truong@liferay.com>
2016-03-30 20:19:53 -0500

    add club and roster member services

--- modules/apps/roster/roster-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/roster/roster-service/src/main/resources/META-INF/sql/indexes.sql
@@ -2,5 +2,10 @@ create index IX_CFFD06FF on FOO_Foo (field2);
 create index IX_B2FCA947 on FOO_Foo (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_905CD589 on FOO_Foo (uuid_[$COLUMN_LENGTH:75$], groupId);
 
+create index IX_3CCD9864 on ROSTER_Club (uuid_[$COLUMN_LENGTH:75$]);
+
 create index IX_C897C98A on ROSTER_Roster (clubId);
 create index IX_B496332F on ROSTER_Roster (uuid_[$COLUMN_LENGTH:75$]);
+
+create index IX_C23C9F99 on ROSTER_RosterMember (rosterId);
+create index IX_84CBE8F5 on ROSTER_RosterMember (uuid_[$COLUMN_LENGTH:75$]);
--- modules/apps/roster/roster-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/roster/roster-service/src/main/resources/META-INF/sql/tables.sql
@@ -14,6 +14,14 @@ create table FOO_Foo (
 	field5 VARCHAR(75) null
 );
 
+create table ROSTER_Club (
+	uuid_ VARCHAR(75) null,
+	clubId LONG not null primary key,
+	createDate DATE null,
+	modifiedDate DATE null,
+	name VARCHAR(75) null
+);
+
 create table ROSTER_Roster (
 	uuid_ VARCHAR(75) null,
 	rosterId LONG not null primary key,
@@ -21,4 +29,13 @@ create table ROSTER_Roster (
 	modifiedDate DATE null,
 	clubId LONG,
 	name VARCHAR(75) null
+);
+
+create table ROSTER_RosterMember (
+	uuid_ VARCHAR(75) null,
+	rosterMemberId LONG not null primary key,
+	createDate DATE null,
+	modifiedDate DATE null,
+	rosterId LONG,
+	contactId LONG
 );
